328a41e5b9b0cfc59a3a69d74a3c89fad12e3219
jlmobbex <javier.lopez@mobbex.com>
2021-10-12 18:31:59 -0300

    Show commission and fee in vendors order view

---
+++ Block/Totals.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Mobbex\Marketplace\Block;
+
+class Totals extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Get current order.
+     *
+     * @return \Vnecoms\VendorsSales\Model\Order
+     */
+    public function getSource()
+    {
+        return $this->getParentBlock()->getSource();
+    }
+
+    public function initTotals()
+    {
+        $order = $this->getSource();
+
+        $finnancialCharge = $order->getMbbxFinnancialCharge();
+        $finalCommission  = $order->getMbbxFinalCommission();
+
+        if ($finnancialCharge != 0) {
+            $this->getParentBlock()->addTotalBefore(
+                new \Magento\Framework\DataObject([
+                    'code' => 'mbbx_finnancial_charge',
+                    'strong' => true,
+                    'value' => $finnancialCharge,
+                    'label' => __('Finnancial Charge'),
+                ]), 
+                'grand_total'
+            );
+        }
+
+        if ($finalCommission != 0) {
+            $this->getParentBlock()->addTotalBefore(
+                new \Magento\Framework\DataObject([
+                    'code' => 'mbbx_final_commission',
+                    'strong' => true,
+                    'value' => $finalCommission,
+                    'label' => __('Final Commission'),
+                ]), 
+                'grand_total'
+            );
+        }
+
+        return $this;
+    }
+}
